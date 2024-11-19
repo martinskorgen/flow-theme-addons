216557d31c8398a5573351d6c23d4bf9aef43751
martinskorgen <mail@martinskorgen.com>
2024-11-19 09:10:08 +0100

    theme name change

---
+++ theme/src/main/java/org/parttio/deemtheme/DeemTheme.java
@@ -0,0 +1,7 @@
+package org.parttio.deemtheme;
+
+public class DeemTheme {
+    public static String getIndetifier() {
+        return "deem";
+    }
+}
--- theme/src/main/java/org/parttio/starpasstheme/StarPassTheme.java
+++
@@ -1,7 +0,0 @@
-package org.parttio.starpasstheme;
-
-public class StarPassTheme {
-    public static String getIndetifier() {
-        return "starpass";
-    }
-}
